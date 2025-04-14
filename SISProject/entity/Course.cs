f763c887f0da31ba63e902182cfcf46dad3eae9f
gokul1234 <156083079+gokul2062004@users.noreply.github.com>
2025-04-14 21:22:43 +0530

    Update Course.cs

--- SISProject/entity/Course.cs
+++ SISProject/entity/Course.cs
@@ -1,28 +1,53 @@
-namespace CarRentalSystem.Entities
+using assignment_2.entity;
+
+using System;
+using System.Collections.Generic;
+
+namespace assignment_2.entity
 {
-    public class Car
+    public class Course
     {
-        public int VehicleID { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public decimal DailyRate { get; set; }
-        public string Status { get; set; }  // "available", "notAvailable"
-        public int PassengerCapacity { get; set; }
-        public int EngineCapacity { get; set; }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public string InstructorName { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
 
-        public Car() { }
-
-        public Car(int vehicleID, string make, string model, int year, decimal dailyRate, string status, int passengerCapacity, int engineCapacity)
+        public Course(int courseId, string courseName, string courseCode, string instructorName)
         {
-            VehicleID = vehicleID;
-            Make = make;
-            Model = model;
-            Year = year;
-            DailyRate = dailyRate;
-            Status = status;
-            PassengerCapacity = passengerCapacity;
-            EngineCapacity = engineCapacity;
+            CourseId = courseId;
+            CourseName = courseName;
+            CourseCode = courseCode;
+            InstructorName = instructorName;
+            Enrollments = new List<Enrollment>();
+        }
+
+        public void AssignTeacher(Teacher teacher)
+        {
+            InstructorName = teacher.FirstName + " " + teacher.LastName;
+            teacher.AssignedCourses.Add(this);
+        }
+
+        public void UpdateCourseInfo(string courseCode, string courseName, string instructor)
+        {
+            CourseCode = courseCode;
+            CourseName = courseName;
+            InstructorName = instructor;
+        }
+
+        public void DisplayCourseInfo()
+        {
+            Console.WriteLine($"ID: {CourseId}, Name: {CourseName}, Code: {CourseCode}, Instructor: {InstructorName}");
+        }
+
+        public List<Enrollment> GetEnrollments()
+        {
+            return Enrollments;
+        }
+
+        public string GetTeacher()
+        {
+            return InstructorName;
         }
     }
 }
