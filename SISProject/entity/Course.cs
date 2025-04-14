6c04d0540d9d24dcdfa1669cc79ce21c5c438753
gokul1234 <156083079+gokul2062004@users.noreply.github.com>
2025-04-14 21:15:19 +0530

    Update Course.cs

--- SISProject/entity/Course.cs
+++ SISProject/entity/Course.cs
@@ -1,1 +1,28 @@
-...
+namespace CarRentalSystem.Entities
+{
+    public class Car
+    {
+        public int VehicleID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal DailyRate { get; set; }
+        public string Status { get; set; }  // "available", "notAvailable"
+        public int PassengerCapacity { get; set; }
+        public int EngineCapacity { get; set; }
+
+        public Car() { }
+
+        public Car(int vehicleID, string make, string model, int year, decimal dailyRate, string status, int passengerCapacity, int engineCapacity)
+        {
+            VehicleID = vehicleID;
+            Make = make;
+            Model = model;
+            Year = year;
+            DailyRate = dailyRate;
+            Status = status;
+            PassengerCapacity = passengerCapacity;
+            EngineCapacity = engineCapacity;
+        }
+    }
+}
